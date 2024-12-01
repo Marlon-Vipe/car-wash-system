9f258b1f3f599d89338f5f69a37670a1d496e8a0
Marlon Villalona <iamvillalona@gmail.com>
2024-11-30 20:07:57 -0400

    added validation in login form

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export default class LoginComponent {
+  
+  private loginForm = inject(FormBuilder)
+
+  public form: FormGroup = this.createForm()
+
+  private createForm(): FormGroup {
+    return this.loginForm.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  login(){
+    console.log("tamo en vivo");
+    this.resetForm()
+  }
+
+  resetForm(){
+    this.form.reset()
+  }
 
 }

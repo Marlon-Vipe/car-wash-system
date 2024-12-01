cc0cfc2d62b6c82817e9cf53847486ec93015e8d
Marlon Villalona <iamvillalona@gmail.com>
2024-12-01 14:25:54 -0400

    added dashboard component

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from "../sidebar/sidebar.component";
+import { DashboardComponent } from "../dashboard/dashboard.component";
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, DashboardComponent, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
